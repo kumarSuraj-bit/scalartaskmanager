317c99c763c6f7bf4c670de0f19a8895279d4080
KumarSuraj <surajk.ug19.ec@nitp.ac.in>
2024-01-05 23:56:35 +0530

    project started

---
+++ src/main/java/com/scaler/scalertaskmanager/ScalertaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.scalertaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalertaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalertaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/scalertaskmanager/ScalertaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.scalertaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalertaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
